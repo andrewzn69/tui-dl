984d48f9f3d458c93b648fa266be5de55defd8bf
Ondřej Zeman <ondrejzeman@zemn.xyz>
2025-04-26 23:23:25 +0200

    add DownloadRow component

---
+++ src/components/downloads/DownloadRow.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Box } from 'ink';
+import { DownloadList } from './DownloadList.js';
+import { DownloadItem } from '../../types/index.js';
+import { DownloadDetails } from './DownloadDetails.js';
+
+interface DownloadRowProps {
+	downloads: DownloadItem[];
+	selectedIndex: number;
+	height: number;
+	width: number;
+}
+
+export const DownloadRow: React.FC<DownloadRowProps> = ({ height, width, downloads, selectedIndex }) => {
+	const listWidth = Math.floor(width * 0.75);
+	const sidebarWidth = width - listWidth;
+
+	return (
+		<Box flexDirection='row' height={height}>
+			<Box width={listWidth} marginRight={2}>
+				<DownloadList
+					downloads={downloads}
+					selectedIndex={selectedIndex}
+					height={height}
+					width={listWidth}
+				/>
+			</Box>
+
+			<Box flexGrow={sidebarWidth}>
+				<DownloadDetails height={height} width={sidebarWidth} />
+			</Box>
+		</Box>
+	);
+};
